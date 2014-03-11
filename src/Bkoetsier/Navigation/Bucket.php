cb0c7db903f5b5bcebf9353842e70faf602e95c6
Bastian Koetsier <bastian@mister-wong.de>
2014-03-11 09:51:38 +0100

    - Bucket implements ArrayIterator in exchange for ArrayAccess & Countable

--- src/Bkoetsier/Navigation/Bucket.php
+++ src/Bkoetsier/Navigation/Bucket.php
@@ -1,7 +1,9 @@
 <?php namespace Bkoetsier\Navigation;
 
 
-class Bucket  implements \ArrayAccess,\Countable{
+use Traversable;
+
+class Bucket  implements \IteratorAggregate{
 
 	protected $items = [];
 	protected $name;
@@ -90,56 +92,11 @@ class Bucket  implements \ArrayAccess,\Countable{
 	}
 
 	/**
-	 * Whether a offset exists
-	 * @param mixed $offset
-	 * An offset to check for.
-	 * @return boolean true on success or false on failure.
-	 * The return value will be casted to boolean if non-boolean was returned.
+	 * Retrieve an external iterator
+	 * @return Traversable An instance of an object implementing Iterator Traversable<
 	 */
-	public function offsetExists($offset)
+	public function getIterator()
 	{
-		return isset($this->items[$offset]);
-	}
-
-	/**
-	 * Offset to retrieve
-	 * @param mixed $offset
-	 * The offset to retrieve.
-	 * @return mixed Can return all value types.
-	 */
-	public function offsetGet($offset)
-	{
-		return $this->items[$offset];
-	}
-
-	/**
-	 * Offset to set
-	 * @param mixed $offset The offset to assign the value to.
-	 * @param mixed $value
-	 * @return void
-	 */
-	public function offsetSet($offset, $value)
-	{
-		$this->items[$offset] = $value;
-	}
-
-	/**
-	 * Offset to unset
-	 * @param mixed $offset The offset to unset.
-	 * @return void
-	 */
-	public function offsetUnset($offset)
-	{
-		unset($this->items[$offset]);
-	}
-
-	/**
-	 * Count elements of an object
-	 * @return int The custom count as an integer.
-	 * The return value is cast to an integer.
-	 */
-	public function count()
-	{
-		return count($this->items);
+		return new \ArrayIterator($this->items);
 	}
 }
