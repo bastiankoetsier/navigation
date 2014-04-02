86faf4b45e32dbbc72e351fb62432bbc604787c0
Bastian Koetsier <bastian@mister-wong.de>
2014-04-02 14:32:19 +0200

    - Commented Breadcumbs

--- src/Bkoetsier/Navigation/Breadcrumbs.php
+++ src/Bkoetsier/Navigation/Breadcrumbs.php
@@ -1,15 +1,22 @@
 <?php namespace Bkoetsier\Navigation;
 
+use Bkoetsier\Navigation\Items\ItemInterface;
 use Bkoetsier\Navigation\Renderer\BreadcrumbRendererInterface;
 
 class Breadcrumbs {
 
+	/**
+	 * @var Bucket
+	 */
 	protected $bucket;
+	/**
+	 * @var ItemInterface[]
+	 */
 	protected $pathItems;
 	/**
 	 * @var Renderer\BreadcrumbRendererInterface
 	 */
-	private $renderer;
+	protected $renderer;
 
 	function __construct(Bucket $bucket,BreadcrumbRendererInterface $renderer)
 	{
@@ -17,12 +24,22 @@ class Breadcrumbs {
 		$this->renderer = $renderer;
 	}
 
+	/**
+	 * Retrieves all Items till $label
+	 * and sets them as $pathItems for rendering
+	 * @param $label
+	 * @return $this
+	 */
 	public function pathTo($label)
 	{
 		$this->pathItems = $this->bucket->pathItems($label);
 		return $this;
 	}
 
+	/**
+	 * Delegates the rendering to BreadcrumbRendererInterface
+	 * @return mixed
+	 */
 	public function render()
 	{
 		return $this->renderer->renderBreadcrumb($this->pathItems);
